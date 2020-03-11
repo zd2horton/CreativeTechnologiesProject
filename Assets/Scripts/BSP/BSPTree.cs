58408b15bdf1a07cc05c9a60819f585f8ec28620
Zach Horton <Zachary2.Horton@live.uwe.ac.uk>
2020-03-11 20:05:47 +0000

    BSP Nearly Complete
    Physical Manifestation and bug fixes required

---
+++ Assets/Scripts/BSP/BSPTree.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSPTree : MonoBehaviour
+{
+    private List<Leaf> partitioningTree;
+    bool finished;
+    bool successfulSplit;
+    LeafSplit splitting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        partitioningTree = new List<Leaf>();
+        splitting = gameObject.GetComponent<LeafSplit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessChild();
+    }
+
+    void ProcessChild()
+    {
+        //initial leaf
+        if (partitioningTree.Count == 0)
+        {
+            Leaf initialArea = new Leaf();
+            initialArea.leafPosition = new Vector2 (0, 0);
+            initialArea.leafWidth = 100;
+            initialArea.leafHeight = 80;
+            partitioningTree.Add(initialArea);
+        }
+
+        successfulSplit = false;
+
+        //while splitting still needs to happen
+        while (finished == false)
+        {
+            //for each leaf
+            for (int i = 0; i < partitioningTree.Count; i++)
+            {
+                Leaf childL = new Leaf();
+                Leaf childR = new Leaf();
+                //if not split already
+                //bool for use here on if true is returned on splitting
+                successfulSplit = splitting.DirectionChoice(partitioningTree[i], ref childL, ref childR);
+
+                if (successfulSplit == true)
+                {
+                    partitioningTree.Add(childL);
+                    partitioningTree.Add(childR);
+                    partitioningTree.Remove(partitioningTree[i]);
+                }
+
+                else if (successfulSplit == false)
+                {
+                    finished = true;
+                }
+
+                childL = null;
+                childR = null;
+            }
+        }
+
+        if (finished == true)
+        {
+            Debug.DebugBreak();
+        }
+    }
+
+    void TreeOutput()
+    {
+        //add child boxes then hide original?
+        //don't need to split original, visualise dimensions of children
+        //just draw final boxes
+        //use data for process and visual representation only at end
+        //separate model from presentation
+        //process current array into finished tree for visuals
+        //process finished tree into visual form
+
+        //keep only required children
+        //bitmap tree
+
+        //output
+    }
+}
---
+++ Assets/Scripts/BSP/LeafSplit.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafSplit : MonoBehaviour
+{
+    //include halls for leaf
+    public float minRoomWidth, minRoomHeight, minLeafSize;
+    bool completedSplit;
+
+    public bool DirectionChoice(Leaf currentLeaf, ref Leaf leftChild, ref Leaf rightChild)
+    {
+        string splitDirection = "";
+        //if there are pre-existing children then return false
+        if (leftChild != null && rightChild != null)
+        {
+            return false;
+        }
+
+        if (currentLeaf.leafHeight > (currentLeaf.leafWidth * 1.40))
+        {
+            //split horizontally
+            splitDirection = "Horizontal";
+        }
+
+        else if (currentLeaf.leafWidth > (currentLeaf.leafHeight * 1.40))
+        {
+            //split vertically
+            splitDirection = "Vertical";
+        }
+
+        else
+        {
+            //otherwise do it randomly
+            int randomPick = Random.Range(1, 3);
+            switch (randomPick)
+            {
+                case 1:
+                    {
+                        splitDirection = "Horizontal";
+                        break;
+                    }
+
+                case 2:
+                    {
+                        splitDirection = "Horizontal";
+                        break;
+                    }
+            }
+        }
+
+        //Take area(leaf)
+        //Random Direction for Cut
+        //Find Random Position for Cut Direction
+        //Split
+        //Document 2 child rooms
+        //place them in tree
+
+        completedSplit = SplitLeaf(splitDirection, currentLeaf, leftChild, rightChild);
+
+        switch (completedSplit)
+        {
+            case true:
+                return true;
+                
+            case false:
+                return false;
+
+            default:
+                return false;
+        }
+    }
+
+    bool SplitLeaf(string cutDirection, Leaf currentLeaf, Leaf lChild, Leaf rChild)
+    {
+        float maxLeafSize = 0.0f;
+        //calculate maximum dimension for leaf
+        if (cutDirection == "Horizontal")
+        {
+            maxLeafSize = currentLeaf.leafHeight - minLeafSize;
+        }
+
+        else if (cutDirection == "Vertical")
+        {
+            maxLeafSize = currentLeaf.leafWidth - minLeafSize;
+        }
+
+        //all canvas used up if maximum dimension for splitting <= minimum size for it
+        if (maxLeafSize <= minLeafSize)
+        {
+            return false;
+        }
+
+        else
+        {
+            //find value to split by through using minimum and maximum split sizes available
+            float splitBy = Random.Range(minLeafSize, maxLeafSize);
+            Debug.Log(splitBy);
+
+            if (cutDirection == "Horizontal")
+            {
+                //create children based on result, same width but reduced height
+                lChild.leafPosition = currentLeaf.leafPosition;
+                lChild.leafWidth = currentLeaf.leafWidth;
+                lChild.leafHeight = splitBy;
+
+                rChild.leafPosition = new Vector2(currentLeaf.leafPosition.x, currentLeaf.leafPosition.y + splitBy);
+                rChild.leafWidth = currentLeaf.leafWidth;
+                rChild.leafHeight = currentLeaf.leafHeight - splitBy;
+
+            }
+
+            else if (cutDirection == "Vertical")
+            {
+                //create children based on result, same height but reduced width
+                lChild.leafPosition = currentLeaf.leafPosition;
+                lChild.leafHeight = currentLeaf.leafHeight;
+                lChild.leafWidth = splitBy;
+
+                rChild.leafPosition = new Vector3(currentLeaf.leafPosition.x + splitBy, currentLeaf.leafPosition.y);
+                rChild.leafHeight = currentLeaf.leafHeight;
+                rChild.leafWidth = currentLeaf.leafWidth - splitBy;
+            }
+
+        }
+        return true;
+    }
+}
